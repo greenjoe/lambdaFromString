23a5cead7f1cabb1affff4e6762bd501623d1317
greenjoe <joegreen@joegreen.pl>
2015-09-12 18:08:34 +0200

    Add some javadocs.

--- src/main/java/pl/joegreen/lambdaFromString/LambdaFactory.java
+++ src/main/java/pl/joegreen/lambdaFromString/LambdaFactory.java
@@ -9,10 +9,16 @@ public class LambdaFactory {
 
     protected final LambdaFactoryConfiguration configuration;
 
+    /**
+     * Returns a LambdaFactory instance with default configuration.
+     */
     public static LambdaFactory get() {
         return new LambdaFactory(LambdaFactoryConfiguration.get());
     }
 
+    /**
+     * Returns a LambdaFactory instance with the given configuration.
+     */
     public static LambdaFactory get(LambdaFactoryConfiguration configuration){
         return new LambdaFactory(configuration);
     }
@@ -21,6 +27,15 @@ public class LambdaFactory {
         this.configuration = configuration;
     }
 
+    /**
+     * Creates lambda from the given code.
+     * @param code source of the lambda as you would write it in Java expression  {TYPE} lambda = ({CODE});
+     * @param typeReference a subclass of TypeReference class with the generic argument representing the type of the lambda
+     *                      , for example  <br> {@code new TypeReference<Function<Integer,Integer>>(){}; }
+     * @param <T> type of the lambda you want to get
+     * @throws LambdaCreationException when anything goes wrong (no other exceptions are thrown including runtimes),
+     * if the exception was caused by compilation failure it will contain a CompilationDetails instance describing them
+     */
     public <T> T createLambda(String code, TypeReference<T> typeReference) throws LambdaCreationException {
         HelperClassSourceProvider helperProvider = configuration.getDefaultHelperClassSourceProvider();
         String helperClassSource = helperProvider.getHelperClassSource(typeReference.toString(), code,
@@ -41,6 +56,11 @@ public class LambdaFactory {
         }
     }
 
+    /**
+     * Convenience wrapper for {@link #createLambda(String, TypeReference)}
+     * which throws unchecked exception instead of checked one.
+     * @see #createLambda(String, TypeReference)
+     */
     public <T> T createLambdaUnchecked(String code, TypeReference<T> type) {
         try {
             return createLambda(code, type);
--- src/main/java/pl/joegreen/lambdaFromString/classFactory/ClassFactory.java
+++ src/main/java/pl/joegreen/lambdaFromString/classFactory/ClassFactory.java
@@ -1,5 +1,12 @@
 package pl.joegreen.lambdaFromString.classFactory;
 
 public interface ClassFactory {
+    /**
+     * Compiles class from its source code and loads it into JVM.
+     * @param fullClassName qualified name of the class
+     * @param sourceCode code of the class
+     * @throws ClassCompilationException in case of compilation failure, it should contain CompilationDetails
+     * instance describing errors if possible
+     */
     Class<?> createClass(String fullClassName, String sourceCode) throws ClassCompilationException;
 }
